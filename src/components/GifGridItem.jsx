d2767701d9babbcc595dff37f521e5a0cc3cc87a
Jose Pardo <josmar.pm19@gmail.com>
2022-02-16 23:26:24 -0500

    First commit

---
+++ src/components/GifGridItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const GifGridItem = ({id, title, url}) => {
+  
+  return (
+    <div 
+      className="card animate__rotateIn"
+    >
+      <img src={url} alt={title}/>
+      <p>{title}</p>
+    </div>
+  )
+}
+
+export default GifGridItem
